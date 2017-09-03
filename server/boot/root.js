7f23c15bdd87a04de2689b47c95aa767b0220c57
srichatala <sri2vas@gmail.com>
2017-09-03 02:42:09 -0400

    connecting to mongo and models added

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,7 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
+  console.log(server);
   router.get('/', server.loopback.status());
   server.use(router);
 };
